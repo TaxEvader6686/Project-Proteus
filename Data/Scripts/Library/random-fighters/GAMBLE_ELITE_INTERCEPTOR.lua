93cf7a3344239ef41ef767d555fc6793e1770840
Supernova-TR-ProPro <nirh95@gmail.com>
2026-07-18 00:12:31 +0200

    Update GAMBLE_ELITE_INTERCEPTOR.lua

--- Data/Scripts/Library/random-fighters/GAMBLE_ELITE_INTERCEPTOR.lua
+++ Data/Scripts/Library/random-fighters/GAMBLE_ELITE_INTERCEPTOR.lua
@@ -19,8 +19,8 @@ return {
         },
         ["UPGUNNED_SHIELDED_ARMORED_INTERCEPTOR_SQUADRON"] = {
             Min_Year = 1, Max_Year = 20, Research = nil
-        },
-        ["ELITE_SHIELDED_ARMORED_INTERCEPTOR_SQUADRON"] = {
+         },
+        ["ION_SHIELDED_ARMORED_INTERCEPTOR_SQUADRON"] = {
             Min_Year = 1, Max_Year = 20, Research = nil
         },
         ["TIE_PHANTOM_SQUADRON"] = {
