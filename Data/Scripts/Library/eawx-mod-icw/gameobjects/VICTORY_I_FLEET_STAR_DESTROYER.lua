e5b955bc987701209cf33a657c6982749a56d80a
Supernova-TR-ProPro <nirh95@gmail.com>
2026-07-17 23:59:55 +0200

    Update VICTORY_I_FLEET_STAR_DESTROYER.lua

--- Data/Scripts/Library/eawx-mod-icw/gameobjects/VICTORY_I_FLEET_STAR_DESTROYER.lua
+++ Data/Scripts/Library/eawx-mod-icw/gameobjects/VICTORY_I_FLEET_STAR_DESTROYER.lua
@@ -1,6 +1,6 @@
 return {
 	Ship_Crew_Requirement = 200,
 	Scripts = {"multilayer", "fighter-spawn"},
-	Fighterflags = {"PROTEUS_OVERRIDE"},
+	FighterFlags = {"PROTEUS_OVERRIDE"},
 	Flags = {FIGHTERINHERIT = "VICTORY_I_STAR_DESTROYER"}
 }
