69388a61d2810ae388952ed62cef765c829b6cc5
Supernova-TR-ProPro <nirh95@gmail.com>
2026-07-17 23:58:56 +0200

    Update REP_DHC.lua

--- Data/Scripts/Library/eawx-mod-icw/gameobjects/REP_DHC.lua
+++ Data/Scripts/Library/eawx-mod-icw/gameobjects/REP_DHC.lua
@@ -1,6 +1,6 @@
 return {
 	Ship_Crew_Requirement = 220,
 	Scripts = {"multilayer", "fighter-spawn"},
-	Fighterflags = {"PROTEUS_OVERRIDE"},
+	FighterFlags = {"PROTEUS_OVERRIDE"},
 	Flags = {FIGHTERINHERIT = "IMPERIAL_DHC"}
 }
